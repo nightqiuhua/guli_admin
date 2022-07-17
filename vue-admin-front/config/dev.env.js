b12e29fc35d023ef548c63e3714dc5bb8f5e0637
nightqiuhua <1320551630@qq.com>
2022-07-17 22:59:05 +0800

    new functions

--- vue-admin-front/config/dev.env.js
+++ vue-admin-front/config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8001"',
+  BASE_API: '"http://localhost:9001"',
+  OSS_PATH: '"https://guli-file.oss-cn-beijing.aliyuncs.com"'
 })
